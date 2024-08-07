8a9dd7c2f87b0881aa6138fbd843ffb50af96982
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2024-08-06 21:41:03 -0300

    fix(ProductaComparison): corrigindo tela

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,8 @@
-import { AddProduct } from './components/AddProduct'
-import { Body } from './components/Body'
-import { Card } from './components/Card'
-import { Text } from './components/Text'
+import ProductComparison from './pages/product-comparison'
 
 function App() {
   return (
-    <>
-      <div className="flex gap-36 ml-24 mt-12">
-        <Text />
-        <div className="flex gap-16 mb-28">
-          <Card
-            texto="Asgaard Sofa"
-            moneyText="Rs. 250,000.00"
-            reviewText="204 Review"
-          />
-          <Card
-            texto="Outdoor Sofa Set"
-            moneyText="Rs. 224,000.00"
-            reviewText="145 Review"
-          />
-        </div>
-        <div className="mt-10w">
-          <AddProduct text=" Choose a Product" />
-        </div>
-      </div>
-      <div className="flex ml-24">
-        <Body />
-      </div>
-    </>
+    <ProductComparison />
   )
 }
 
