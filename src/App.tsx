fddb7b79b389d165a0c8edd680b4dd5604fa049b
LucasFiorentin <lucasfiorentin2008@gmail.com>
2024-08-06 21:57:44 -0300

    Ajustando pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,7 @@
-import { Header } from './components/Header'
-import { Cards } from './components/Cards'
-import { Filter } from './components/Filter'
-import { Cards2 } from './components/Cards2'
-import { Buttons } from './components/Buttons'
+import Shop from './pages/Shop'
 
-const Card = new Array(3).fill(null)
 function App() {
-  return (
-    <>
-      <footer />
-      <Header />
-      <Filter />
-      <div className="flex items-center justify-center pt-16 pb-5">
-        <Cards />
-      </div>
-      <div className="flex flex-col items-center justify-center gap-y-5">
-        {Card.map(() => (
-          <Cards2 />
-        ))}
-      </div>
-      <div className="pt-10">
-        <Buttons />
-      </div>
-    </>
-  )
+  return <Shop />
 }
 
 export default App
---
+++ src/pages/Shop.tsx
@@ -0,0 +1,26 @@
+import { Cards } from '../components/Cards'
+import { Filter } from '../components/Filter'
+import { Cards2 } from '../components/Cards2'
+import { Buttons } from '../components/Buttons'
+
+const Card = new Array(3).fill(null)
+function Shop() {
+  return (
+    <>
+      <Filter />
+      <div className="flex items-center justify-center pt-16 pb-5">
+        <Cards />
+      </div>
+      <div className="flex flex-col items-center justify-center gap-y-5">
+        {Card.map(() => (
+          <Cards2 />
+        ))}
+      </div>
+      <div className="pt-10">
+        <Buttons />
+      </div>
+    </>
+  )
+}
+
+export default Shop
