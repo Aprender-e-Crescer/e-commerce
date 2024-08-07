ec69ae9ac5f736eb9069e2a925413d3d80cb89bb
Vinicius Raitz Pilati <vini1718a@gmail.com>
2024-08-06 23:29:36 -0300

    add componentes e interface dos inputs da página

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
+import { CheckoutPage } from './pages/checkout-page'
 import ProductComparison from './pages/product-comparison'
 
 function App() {
   return (
+    <>
     <ProductComparison />
+    <CheckoutPage />
+    </>
   )
 }
 
 export default App
+
