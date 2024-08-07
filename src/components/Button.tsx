fadee3d8c77fe6110a33757f52cd9a144c31d46b
JonatasFiorentin <jonatasfiorentin@gmail.com>
2024-08-06 21:42:08 -0300

    Ajustando props Button

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,7 +1,7 @@
-export function Button(props) {
+export function Button({ text }: { text: string }) {
   return (
     <div className="flex items-center justify-center border-[2px] border-[#B88E2F]  hover:bg-[#B88E2F] leading-4 font-semibold text-smp-2 p-2 hover:text-white ease-in duration-300">
-      <p>{props.text}</p>
+      <p>{text}</p>
     </div>
   )
 }
