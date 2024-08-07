e74dec17ba013f9b4e8946308f5c6fe5f37a3082
LucasFiorentin <lucasfiorentin2008@gmail.com>
2024-08-06 21:45:47 -0300

    Adicionando tela shop

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Header } from './components/Header'
+import { Cards } from './components/Cards'
+import { Filter } from './components/Filter'
+import { Cards2 } from './components/Cards2'
+import { Buttons } from './components/Buttons'
 
+const Card = new Array(3).fill(null)
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <footer />
+      <Header />
+      <Filter />
+      <div className="flex items-center justify-center pt-16 pb-5">
+        <Cards />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div className="flex flex-col items-center justify-center gap-y-5">
+        {Card.map(() => (
+          <Cards2 />
+        ))}
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="pt-10">
+        <Buttons />
+      </div>
     </>
   )
 }
