2bdb0c46a8ef3595f53a57927d15068152314df2
alexandre1921 <alexandre_leonardo2010@hotmail.com>
2024-08-08 21:27:49 -0300

    corrigindo props

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,32 +1,190 @@
-import { Filter } from '../components/Filter'
-import { Cards } from '../components/Cards'
 import { Buttons } from '../components/Buttons'
 import image1 from '../assets/Images.png'
-import image3 from '../assets/Images (1).png'
-import image4 from '../assets/Images (2).png'
-import image2 from '../assets/Images (3).png'
+import { Card } from '@/components/Card'
 
-const hoverStates = [true, true, false, false]
+const linhasDeCards = [
+  [
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    },
+    {
+      descrição: 'Teste', 
+      estaComHover: true,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    },
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    },
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    }
+  ],
+  [
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    },
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    },
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    },
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    }
+  ],
+  [
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    },
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    },
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    },
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    }
+  ],
+  [
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    },
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    },
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    },
+    {
+      descrição: 'Teste', 
+      estaComHover: false,
+      titulo: 'Produto bom',
+      valorAtual: '6000',
+      img: image1,
+      porcentagem: '90',
+      valorPassado: '900',
+    }
+  ],  
+]
 
 function Shop() {
   return (
     <>
       <Filter />
       <div className="flex flex-col items-center justify-center gap-y-5 pt-10">
-        {hoverStates.map((estaComHover) => (
-          <Cards
-            estaComHover={estaComHover}
-            img1={image1}
-            img2={image2}
-            img3={image3}
-            img4={image4}
-            titulo="styreline"
-            descrição="teste"
-            valorAtual="Rp.120.000"
-            valorPassado="Rp.100.000"
-            porcentagem="-30%"
-          />
-        ))}
+      {linhasDeCards.map((linha) => (
+        <div className="flex gap-x-12">
+          {linha.map(({ 
+            descrição,
+            estaComHover,
+            img,
+            porcentagem,
+            titulo,
+            valorAtual,
+            valorPassado
+          }) => (
+            <Card
+              descrição={descrição}
+              estaComHover={estaComHover}
+              titulo={titulo}
+              valorAtual={valorAtual}
+              img={img}
+              porcentagem={porcentagem}
+              valorPassado={valorPassado}
+            />
+          ))}
+        </div>
+      ))}
       </div>
       <div className="pt-10">
         <Buttons />
